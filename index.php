69cc00c7073b39e2710c7ad72658ac5062d274dc
Antoine Augusti <antoine@augusti.fr>
2013-07-02 13:42:03 +0200

    Change the home image

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include "header.php";
 ?>
 
 	<div id="single_image">
-		<a href="https://www.facebook.com/Lia.s.Images" target="_blanck"><span class="accueil hover" style="background-image:url(/images/accueil.jpg)"></span></a>
+		<span class="accueil" style="background-image:url(/images/accueil.jpg)"></span>
 	</div><!-- END SINGLE IMAGE -->
 
 <?php
