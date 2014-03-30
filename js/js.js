53d82fd2f30de4751310b3f661573285fb8b6151
Antoine Augusti <antoine@augusti.fr>
2014-03-30 11:14:10 +0200

    Faster scroll + indentation

--- js/js.js
+++ js/js.js
@@ -1,35 +1,33 @@
-<!--
 function clickIE4() {
-    if (event.button == 2) {
-        return false;
-    }
+	if (event.button == 2) {
+		return false;
+	}
 }
 
 function clickNS4(e) {
-    if (document.layers || document.getElementById && !document.all) {
-        if (e.which == 2 || e.which == 3) {
-            return false;
-        }
-    }
+	if (document.layers || document.getElementById && !document.all) {
+		if (e.which == 2 || e.which == 3) {
+			return false;
+		}
+	}
 }
 
 if (document.layers) {
-    document.captureEvents(Event.MOUSEDOWN);
-    document.onmousedown = clickNS4;
+	document.captureEvents(Event.MOUSEDOWN);
+	document.onmousedown = clickNS4;
 } else if (document.all && !document.getElementById) {
-    document.onmousedown = clickIE4;
+	document.onmousedown = clickIE4;
 }
 
-document.oncontextmenu = new Function("return false")
+document.oncontextmenu = new Function("return false");
 
-// -->
 $(function() {
 
    $("#gallery").mousewheel(function(event, delta) {
 
-      this.scrollLeft -= (delta * 100);
-    
-      event.preventDefault();
+	  this.scrollLeft -= (delta * 150);
+	
+	  event.preventDefault();
 
    });
 
@@ -41,63 +39,66 @@ var contact_mail_sent = "L'email a bien été envoyé !";
 
 $(document).ready(function()
 {
-    $("#gallery,#contact,#single_image,#contactForm,#legal").css("display", "none");
-    $("#gallery,#contact,#single_image,#legal").fadeIn(1500);
-    $("#contactForm").fadeIn(2500);
+	var hauteurEcran = $(window).height() - 150;
+	console.log(hauteurEcran);
+	$("#contactForm,#legal").css("display", "none");
+	$("#legal").fadeIn(1500);
+	$("#contactForm").fadeIn(2500);
+	$("#gallery, #gallery ul li span, #single_image").css({ 'height': hauteurEcran + "px" });
 
-    $("#contactForm").submit(function(event)
-    {
-        event.preventDefault();
+	$("#contactForm").submit(function(event)
+	{
+		event.preventDefault();
 
-        $('#notification').html('');
-        $('#notification').html('<div class="alert">Chargement...</div>').slideDown();
+		$('#notification').html('');
+		$('#notification').html('<div class="alert">Chargement...</div>').slideDown();
 
-        // Get variables
-        var sujet = $("#inputSujet").val();
-        var nom = $("#inputNom").val();
-        var email = $("#inputEmail").val();
-        var message = $("#textareaMessage").val();
-        var copie = $("#checkboxCopie").val();
+		// Get variables
+		var sujet = $("#inputSujet").val();
+		var nom = $("#inputNom").val();
+		var email = $("#inputEmail").val();
+		var message = $("#textareaMessage").val();
+		var copie = $("#checkboxCopie").val();
 
-        $.post("/ajax/sendmail.php", {
-            sujet: sujet,
-            nom: nom,
-            email: email,
-            message: message,
-            copie: copie
-        },
+		$.post("/ajax/sendmail.php", {
+			sujet: sujet,
+			nom: nom,
+			email: email,
+			message: message,
+			copie: copie
+		},
 
-        function(data)
-        {
-            // Empty form
-            if(data == 1)
-            {
-                div_response = '<div class="alert">' + contact_empty_form + '</div>';
-            }
-            // Wrong captcha
-            else if(data == 2)
-            {
-                div_response = '<div class="alert">' + contact_wrong_captcha + '</div>';
+		function(data)
+		{
+			// Empty form
+			if(data == 1)
+			{
+				div_response = '<div class="alert">' + contact_empty_form + '</div>';
+			}
+			// Wrong captcha
+			else if(data == 2)
+			{
+				div_response = '<div class="alert">' + contact_wrong_captcha + '</div>';
 
-                // Focus the wrong input
-                $("#inputCaptcha").val('').focus();
-            }
-            // Mail sent
-            else if(data == 3)
-            {
-                div_response = '<div class="alert alert-success">' + contact_mail_sent + '</div>';
+				// Focus the wrong input
+				$("#inputCaptcha").val('').focus();
+			}
+			// Mail sent
+			else if(data == 3)
+			{
+				div_response = '<div class="alert alert-success">' + contact_mail_sent + '</div>';
 
-                $("#contactForm").fadeOut(4000);
-            }
-            // Unknow error
-            else if(data == 4)
-            {
-                div_response = '<div class="alert">Error</div>';
-            }
+				$("#contactForm").fadeOut(4000);
+			}
+			// Unknow error
+			else if(data == 4)
+			{
+				div_response = '<div class="alert">Error</div>';
+			}
 
-            $('#notification').html(div_response).delay(3000).slideUp();
-        });
+			$('#notification').html(div_response).delay(3000).slideUp();
+		});
 
-        return false;
-    });
+		return false;
+	});
 });
